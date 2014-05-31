ef7c1c8accfaddd298498005ec89596a86dbb221
fusionjack <dogfight60-fusionjack@yahoo.de>
2014-05-31 02:07:11 +0200

    Compile with -Os to fix broken exfat
    Thanks to @kantjer for his idea that the exfat problem lies on mount.exfat
    
    Change-Id: Ic6492b16452960f6fb0a6a8c5f00fa7e022125d5

--- fuse/Android.mk
+++ fuse/Android.mk
@@ -3,7 +3,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := libexfat_mount
 LOCAL_MODULE_TAGS := optional
-LOCAL_CFLAGS = -D_FILE_OFFSET_BITS=64
+LOCAL_CFLAGS = -Os -D_FILE_OFFSET_BITS=64
 LOCAL_SRC_FILES = main.c 
 LOCAL_C_INCLUDES += $(LOCAL_PATH) \
 					external/exfat/libexfat \
--- libexfat/Android.mk
+++ libexfat/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := libexfat
 LOCAL_MODULE_TAGS := optional
-LOCAL_CFLAGS = -D_FILE_OFFSET_BITS=64
+LOCAL_CFLAGS = -Os -D_FILE_OFFSET_BITS=64
 LOCAL_SRC_FILES = cluster.c io.c log.c lookup.c mount.c node.c time.c utf.c utils.c 
 LOCAL_C_INCLUDES += $(LOCAL_PATH) \
 LOCAL_SHARED_LIBRARIES += libc
