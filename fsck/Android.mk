3ad52280af181dcf6399408669bb62a6c3846615
Eduard Alfonso <xsynergy510x@gmail.com>
2014-08-15 14:33:36 -0700

    Revert "Kantjer: Fix for exFat sdcards"
    This reverts commit 5a8b7e964a1ff41a315ded50ca598d552ca96d13.

--- fsck/Android.mk
+++ fsck/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := libexfat_fsck
 LOCAL_MODULE_TAGS := optional
-LOCAL_CFLAGS = -Os -D_FILE_OFFSET_BITS=64
+LOCAL_CFLAGS = -D_FILE_OFFSET_BITS=64
 LOCAL_SRC_FILES =  main.c
 LOCAL_C_INCLUDES += $(LOCAL_PATH) \
 					external/exfat/libexfat \
